238934291626bce3c941f8602fe8de1044e69769
John Mortensen <jmortensen@powayusd.com>
2021-03-19 22:49:23 -0700

    Add course materials.

---
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -0,0 +1,34 @@
+package com.nighthawk.csa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class MainController {
+
+    @GetMapping("/course/prep")
+    public String coursePrep(Model model) {
+        model.addAttribute("url", "https://padlet.com/jmortensen7/csatime");
+        return "routes/course";
+    }
+
+    @GetMapping("/course/tri1")
+    public String courseTri1(Model model) {
+        model.addAttribute("url", "https://padlet.com/jmortensen7/jho9v5wc4p9jgyn2");
+        return "routes/course";
+    }
+
+    @GetMapping("/course/tri2")
+    public String courseTri2(Model model) {
+        model.addAttribute("url", "https://padlet.com/jmortensen7/csatime1_2");
+        return "routes/course";
+    }
+
+    @GetMapping("/course/tri3")
+    public String courseTri3(Model model) {
+        model.addAttribute("url", "https://padlet.com/jmortensen7/csa2021t3");
+        return "routes/course";
+    }
+
+}
