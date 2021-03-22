3e98e82212fbf85fe925bd1af42d193e7ada9317
John Mortensen <jmortensen@powayusd.com>
2021-03-21 19:20:33 -0700

    Dog hair

--- src/main/java/com/nighthawk/csa/Main.java
+++ src/main/java/com/nighthawk/csa/Main.java
@@ -1,9 +1,7 @@
 package com.nighthawk.csa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class Main {
