90563dca664887050667d14ab82ad8687492003a
John Mortensen <jmortensen@powayusd.com>
2021-03-21 18:26:25 -0700

    Error Handling

--- src/main/java/com/nighthawk/csa/Main.java
+++ src/main/java/com/nighthawk/csa/Main.java
@@ -1,11 +1,12 @@
 package com.nighthawk.csa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
