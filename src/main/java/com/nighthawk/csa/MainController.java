624da500b432e40cdebb1af81c30fc446dc70be4
ridhimainukurti <rinukurti@gmail.com>
2021-12-10 05:07:00 -0800

    created about me page with web api

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.ui.Model;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.text.ParseException;
+import java.util.HashMap;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class MainController {
@@ -80,7 +92,23 @@ public class MainController {
     }
 
     @GetMapping("/ridhima")   // GET request
-    public String Ridhima() {
+    public String space(Model model) throws IOException, InterruptedException, ParseException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://quotes15.p.rapidapi.com/quotes/random/"))
+                .header("x-rapidapi-host", "quotes15.p.rapidapi.com")
+                .header("x-rapidapi-key", "e25c6452c9msh8ae4033709e74bbp16b06cjsnb8c921a2f043")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        //convert response.body() to java hash map
+        var quotes = new ObjectMapper().readValue(response.body(), HashMap.class);
+
+
+        //pass stats to view
+        model.addAttribute("quotes", quotes);
+
         return "individual/ridhima";
     }
 
