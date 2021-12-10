6a3dfdd735ede858567721c9ac056edb0ee55228
mistylavender <89210459+mistylavender@users.noreply.github.com>
2021-12-10 08:20:49 -0800

    natasha mask pictures

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -8,18 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.ui.Model;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.text.ParseException;
-import java.util.HashMap;
-
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class MainController {
@@ -76,6 +64,11 @@ public class MainController {
         return "algorithm/snake";
     }
 
+    @GetMapping("/ava")   // GET request
+    public String Ava() {
+        return "individual/ava";
+    }
+
     @GetMapping("/sarah")   // GET request
     public String Sarah() {
         return "individual/sarah";
@@ -87,23 +80,7 @@ public class MainController {
     }
 
     @GetMapping("/ridhima")   // GET request
-    public String space(Model model) throws IOException, InterruptedException, ParseException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://quotes15.p.rapidapi.com/quotes/random/"))
-                .header("x-rapidapi-host", "quotes15.p.rapidapi.com")
-                .header("x-rapidapi-key", "e25c6452c9msh8ae4033709e74bbp16b06cjsnb8c921a2f043")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-
-        //convert response.body() to java hash map
-        var quotes = new ObjectMapper().readValue(response.body(), HashMap.class);
-
-
-        //pass stats to view
-        model.addAttribute("quotes", quotes);
-
+    public String Ridhima() {
         return "individual/ridhima";
     }
 
