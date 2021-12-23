6d6d235cfc999ecce64d68bafe55cb7786ca60a3
John Mortensen <jmortensen@powayusd.com>
2021-12-22 19:52:04 -0800

    add Class to be used with FRQ2

---
+++ src/main/java/com/nighthawk/csa/algorithm/StringSequence.java
@@ -0,0 +1,95 @@
+package com.nighthawk.csa.algorithm;
+import java.util.ArrayList;
+
+public class StringSequence {
+    private String sequence = null;
+    private final ArrayList<String> events;
+
+    /* Constructor
+     */
+    public StringSequence(String sequence) {
+        this.events = new ArrayList<>();
+        this.setSequence(sequence);
+    }
+
+    /* Initialize or Swap sequence
+     */
+    public void setSequence(String sequence) {
+        this.events.add(
+                (this.sequence != null)     // ternary operator usage to avoid null sequence
+                    ? "Sequence changed from: " + this.sequence +" to: " + sequence
+                    : "Set up sequence object: " + sequence);
+
+        // replace new sequence over existing sequence in object
+        this.sequence = sequence;
+    }
+
+    /* Insert "segment" at "position"
+     */
+    public void insertSequenceAt(String segment, int position) {
+        this.events.add ( "Insert into: " + this.sequence + " segment: " + segment +" at " + position );
+
+        // create a gap to insert segment
+        this.setSequence(
+                this.sequence.substring(0, position) +
+                segment +
+                this.sequence.substring(position)
+        );
+    }
+
+    /* Replace "out" segment with "in" segment
+     */
+    public void replaceSequence(String out, String in) {
+        this.events.add ( "Replace: " + sequence + " old segment: " + out +" with new segment " + in );
+
+        // find gap for out segment
+        int index1 = this.sequence.indexOf(out);
+        int index2 = index1 + out.length();
+
+        // build front part and back part of new segemtn
+        String front = this.sequence.substring(0, index1);
+        String back = this.sequence.substring(index2); // automatically goes to end of string
+
+        // concatenate "in" between front and back parts of original
+        this.setSequence(
+                front +
+                in +
+                back
+        );
+    }
+
+    /* StringSequence object reference will return value of sequence attribute
+     */
+    @Override
+    public String toString() {
+        return this.sequence;
+    }
+
+    /* Console output helper method
+     */
+    public void printHistory() {
+        for (String event: events)
+            System.out.println(event);
+        System.out.println("Current: " + this);
+    }
+
+    /* Class tester method
+     */
+    public static void main(String[] args) {
+        // Test1 construct object
+        StringSequence sequence = new StringSequence("0101 0101 0101");
+
+        // Test2 change content of object
+        sequence.setSequence("0011 0011 0011");
+
+        // Test3 insert into content of object
+        sequence.insertSequenceAt("1111 1111", 4);
+
+        // Test4 replace sequence with one that has proper spacing
+        sequence.replaceSequence("00111111", "0011 1111");
+
+        // History of events
+        sequence.printHistory();
+    }
+
+}
