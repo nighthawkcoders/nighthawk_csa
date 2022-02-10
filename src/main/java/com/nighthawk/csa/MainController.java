1164d5c319093934a9a3bcb00bc1294fdb19d5e0
Sarah Xie <sarahwxie@gmail.com>
2022-02-10 08:43:05 -0800

    Update the view - addedd padding so it looks consistent and better

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -176,8 +176,6 @@ public class MainController {
                     "        }";
 
         }
-
-
         // message = frq2;
         model.addAttribute("message", message);
         return "individual/message";
