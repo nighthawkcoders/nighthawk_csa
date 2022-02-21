d9bd9179866f1fd8eee3a2095b39dc06fb177a7a
John Mortensen <jmortensen@powayusd.com>
2022-02-21 06:45:48 -0800

    Start calculator

---
+++ src/main/java/com/nighthawk/csa/hacks/squirrel/Calculator.java
@@ -0,0 +1,9 @@
+package com.nighthawk.csa.hacks.squirrel;
+
+public class Calculator {
+    public static void main(String[] args) {
+        String expression = "100 + 200";
+
+        System.out.println(expression);
+    }
+}
