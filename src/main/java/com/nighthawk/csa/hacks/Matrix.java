3a4848150ae4ea5333a8c78b1dc30c782ef1dc09
John Mortensen <jmortensen@powayusd.com>
2022-02-21 21:42:23 -0800

    matrix printing class

--- src/main/java/com/nighthawk/csa/hacks/Matrix.java
+++ src/main/java/com/nighthawk/csa/hacks/Matrix.java
@@ -1,12 +1,15 @@
 package com.nighthawk.csa.hacks;
 
+// matrix class is used to store and format the output of a matrix
 public class Matrix {
     private final int[][] matrix;
 
+    // store matrix
     public Matrix(int[][] matrix) {
         this.matrix = matrix;
     }
 
+    // nest for loops to format output of a matrix
     public String toString() {
         // construct output of matrix using for loops
         // outer loop for row
@@ -14,30 +17,33 @@ public class Matrix {
         for (int[] row : matrix) {
             // inner loop for column
             for (int cell : row) {
-                output.append(cell).append(" ");
+                output.append((cell==-1) ? " " : String.format("%x",cell)).append(" ");
             }
             output.append("\n"); // new line
         }
         return output.toString();
     }
 
-    static int[][] matix3x3() {
-        // declare and initialize a 3x3 matrix
-        return new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    // declare and initialize a matrix for a keypad
+    static int[][] keypad() {
+        return new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, {-1, 0, -1} };
     }
 
+    // declare and initialize a random length arrays
     static int[][] numbers() {
-        // declare and initialize a 3x3 matrix
         return new int[][]{ { 0, 1 },
                 { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
                 { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
     }
 
+    // tester method for matrix formatting
     public static void main(String[] args) {
-        Matrix m0 = new Matrix(matix3x3());
+        Matrix m0 = new Matrix(keypad());
+        System.out.println("Keypad:");
         System.out.println(m0);
 
         Matrix m1 = new Matrix(numbers());
+        System.out.println("Numbers Systems:");
         System.out.println(m1);
     }
 
