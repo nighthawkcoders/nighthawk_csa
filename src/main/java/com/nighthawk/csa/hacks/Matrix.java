deef24b04179258ad2e471315c6d63207f0871a6
John Mortensen <jmortensen@powayusd.com>
2022-02-24 13:44:17 -0800

    Updating code for matrix challenge.

--- src/main/java/com/nighthawk/csa/hacks/Matrix.java
+++ src/main/java/com/nighthawk/csa/hacks/Matrix.java
@@ -24,6 +24,20 @@ public class Matrix {
         return output.toString();
     }
 
+    // print it backwards matrix
+    public String reverse() {
+        // outer loop starting at end row
+        StringBuilder output = new StringBuilder();
+        for (int i = matrix.length;  0 < i; i--) {
+            // inner loop for column
+            for (int j =  matrix[i-1].length; 0 < j; j--) {
+                output.append((matrix[i-1][j-1]==-1) ? " " : String.format("%x",matrix[i-1][j-1])).append(" ");
+            }
+            output.append("\n"); // new line
+        }
+        return output.toString();
+    }
+
     // declare and initialize a matrix for a keypad
     static int[][] keypad() {
         return new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, {-1, 0, -1} };
@@ -41,10 +55,14 @@ public class Matrix {
         Matrix m0 = new Matrix(keypad());
         System.out.println("Keypad:");
         System.out.println(m0);
+        System.out.println(m0.reverse());
+
 
         Matrix m1 = new Matrix(numbers());
         System.out.println("Numbers Systems:");
         System.out.println(m1);
+        System.out.println(m1.reverse());
+
     }
 
 }
