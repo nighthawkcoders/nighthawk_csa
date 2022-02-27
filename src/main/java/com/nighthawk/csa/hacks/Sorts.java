99e27f3d66b1e3c22ecf07fd2b89cc080a0b9a32
John Mortensen <jmortensen@powayusd.com>
2022-02-26 19:37:41 -0800

    Working on analytics

--- src/main/java/com/nighthawk/csa/hacks/Sorts.java
+++ src/main/java/com/nighthawk/csa/hacks/Sorts.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Sorts {
-    private ArrayList<Integer> data = new ArrayList<>();
-    private Duration timeElapsed;
+    private final ArrayList<Integer> data = new ArrayList<>();
+    private final Duration timeElapsed;
 
     public Sorts(int size) {
         Instant start = Instant.now();  // time capture -- start
         // build an array
         for (int i = 0; i < size; i++) {
-            data.add((int)(Math.random() * size));
+            data.add((int)(Math.random() * (size+1)));
         }
         data.sort(Comparator.naturalOrder());
         Instant end = Instant.now();    // time capture -- end
@@ -30,9 +30,17 @@ public class Sorts {
 
 
     public static void main(String[] args) {
-        Sorts s = new Sorts(5000);
-        System.out.println(s.getData());
-        System.out.println("Nanoseconds: " +s.getTimeElapsed());
+        int sum=0, time=0, TIMES=10, SIZE=5000;
+
+        for(int i=0; i< TIMES; i++) {
+            Sorts s = new Sorts(SIZE);
+            for(int j = 0; j<s.getData().size(); j++) sum += s.getData().get(j);
+            System.out.println("Average random: " + sum / ((i+1)*SIZE));
+            System.out.println("Nanoseconds: " + s.getTimeElapsed());
+            time += s.getTimeElapsed();
+        }
+        System.out.println("Average random: " + sum / (TIMES*SIZE));
+        System.out.println("Average Nanosecond: " + time / TIMES);
     }
 
 }
