843b1c9daf604d0c469003e1c2ee36d5457b4071
John Mortensen <jmortensen@powayusd.com>
2022-03-10 10:48:10 -0800

    progress using runnable

--- src/main/java/com/nighthawk/csa/hacks/Menu.java
+++ src/main/java/com/nighthawk/csa/hacks/Menu.java
@@ -4,37 +4,133 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Menu {
-    String title;
-    Runnable action;
+/**
+ * Menu: custom implementation
+ * @author     John Mortensen
+ *
+ * Uses String to contain Title for Option
+ * Uses Runnable to store Class-Method to be run when Title is selected
+ */
 
-    public Menu(String title, Runnable action) {
+// The Menu Class has a HashMap of Menu Rows
+public class Menu {
+    // Format
+    // Key {0, 1, 2, ...} created based on order of input menu
+    // Value {MenuRow0, MenuRow1, MenuRow2,...} each corresponds to key
+    // MenuRow  {<Exit,Noop>, Option1, Option2, ...}
+    Map<Integer, MenuRow> menu = new HashMap<>();
+
+    /**
+     *  Constructor for Menu,
+     *
+     * @param  rows,  is the row data for menu.
+     */
+    public Menu(MenuRow[] rows) {
+        int i = 0;
+        for (MenuRow row : rows) {
+            // Build HashMap for lookup convenience
+            menu.put(i++, new MenuRow(row.getTitle(), row.getAction()));
+        }
+    }
+
+    /**
+     *  Get Row from Menu,
+     *
+     * @param  i,  HashMap key (k)
+     *
+     * @return  MenuRow, the selected menu
+     */
+    public MenuRow get(int i) {
+        return menu.get(i);
+    }
+
+    /**
+     *  Iterate and print rows in HashMap
+     *
+     */
+    public void print() {
+        for (Map.Entry<Integer, MenuRow> pair : menu.entrySet()) {
+            System.out.println(pair.getKey() + " ==> " + pair.getValue().getTitle());
+        }
+    }
+}
+
+// The MenuRow Class has title and action for individual line item in menu
+class MenuRow {
+    String title;       // menu item title
+    Runnable action;    // menu item action, using Runnable
+
+    /**
+     *  Constructor for MenuRow,
+     *
+     * @param  title,  is the description of the menu item
+     * @param  action, is the run-able action for the menu item
+     */
+    public MenuRow(String title, Runnable action) {
         this.title = title;
         this.action = action;
     }
 
+    /**
+     *  Getters
+     */
     public String getTitle() {
         return this.title;
     }
-
     public Runnable getAction() {
         return this.action;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Map<Integer, Menu> menu = new HashMap<>();
-
-        menu.put(1, new Menu("Swap", () -> IntByReference.main(null) ) );
-        menu.put(2, new Menu("Matrix", () -> Matrix.main(null) ) );
-
-        System.out.println("Menu:");
-        for (Map.Entry<Integer, Menu> pair : menu.entrySet()) {
-            System.out.println(pair.getKey() + " ==> " + pair.getValue().getTitle());
-        }
-
-        int input = sc.nextInt();
-        Menu m = menu.get(input);
-        m.getAction().run();
+    /**
+     *  Runs the action using Runnable (.run)
+     */
+    public void run() {
+        action.run();
     }
 }
+
+// The Main Class illustrates initializing and using Menu with Runnable action
+class Main {
+    /**
+     *  Menu Control Example
+     */
+    public static void main(String[] args) {
+        // Row initialize
+        MenuRow[] rows = new MenuRow[]{
+                new MenuRow("Exit", () -> main(null)),
+                new MenuRow("Swap", () -> IntByReference.main(null)),  // lambda style, () -> to point to Class.Method
+                new MenuRow("Matrix", () -> Matrix.main(null))
+        };
+
+        // Menu construction
+        Menu menu = new Menu(rows);
+
+        // Run menu forever, exit condition contained in loop
+        while (true) {
+            System.out.println("Hacks Menu:");
+            // print rows
+            menu.print();
+
+            // Scan for input
+            try {
+                Scanner sc = new Scanner(System.in);
+                int selection = sc.nextInt();
+
+                // menu action
+                try {
+                    MenuRow row = menu.get(selection);
+                    // stop menu condition
+                    if (row.getTitle().equals("Exit"))
+                        return;
+                    // run option
+                    row.run();
+                } catch (Exception e) {
+                    System.out.printf("Invalid selection %d\n", selection);
+                }
+            } catch (Exception e) {
+                System.out.println("Not a number");
+            }
+        }
+    }
+}
+
