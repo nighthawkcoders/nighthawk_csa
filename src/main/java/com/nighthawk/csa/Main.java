5827025bec8c37c911417823b7c259c68fc8c3fe
John Mortensen <jmortensen@powayusd.com>
2022-05-04 10:57:30 -0700

    role data defaults

--- src/main/java/com/nighthawk/csa/Main.java
+++ src/main/java/com/nighthawk/csa/Main.java
@@ -20,13 +20,18 @@ public class Main {
         return new BCryptPasswordEncoder();
     }
 
-    // Sets up preliminary data
+    // CommandLineRunner provides options to up preliminary data
     @Bean
     CommandLineRunner run(SqlRepository sql) { // testing the database with role adds etc
         return args -> {
+            // make sure every record added has Default encrypted password and ROLE_STUDENT
             sql.defaults("123querty", "ROLE_STUDENT");
-            sql.addRoleToPerson("jmort1021@gmail.com", "ROLE_TEACHER");
 
+            // add privileged roles
+            sql.addRoleToPerson("jmort1021@gmail.com", "ROLE_TEACHER");
+            sql.addRoleToPerson("jmort1021@gmail.com", "ROLE_ADMIN");
+
+            // output to console
             System.out.println(sql.listAll());
             System.out.println(sql.listAllRoles());
         };
