1993671d8216776cae03e80486848d187e05ba77
John Mortensen <jmortensen@powayusd.com>
2022-05-12 06:28:39 -0700

    Add Role checks in Runner

--- src/main/java/com/nighthawk/csa/mvc/database/ModelInit.java
+++ src/main/java/com/nighthawk/csa/mvc/database/ModelInit.java
@@ -1,15 +1,30 @@
 package com.nighthawk.csa.mvc.database;
 
+import com.nighthawk.csa.mvc.database.role.Role;
+import com.nighthawk.csa.mvc.database.role.RoleJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component // Scans Application for ModelInit Bean, this detects CommandLineRunner
 public class ModelInit {
-    // The run() method of the MyRunner will be executed after the application starts
+    // Inject repositories
+    @Autowired RoleJpaRepository roleJpaRepository;
+    @Autowired ModelRepository modelRepository;
+
     @Bean
-    CommandLineRunner run(ModelRepository modelRepository) {
+    CommandLineRunner run() {  // The run() method will be executed after the application starts
         return args -> {
+            // Fail safe data validations
+
+            // make sure Role database is populated with defaults
+            String[] roles = {"ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN", "ROLE_TESTER"};
+            for (String role : roles) {
+                if (roleJpaRepository.findByName(role) == null)
+                    roleJpaRepository.save(new Role(null, role));
+            }
+
             // make sure every record added has a Default encrypted password and ROLE_STUDENT
             modelRepository.defaults("123querty", "ROLE_STUDENT");
 
@@ -17,7 +32,7 @@ public class ModelInit {
             modelRepository.addRoleToPerson("jmort1021@gmail.com", "ROLE_TEACHER");
             modelRepository.addRoleToPerson("jmort1021@gmail.com", "ROLE_ADMIN");
 
-            // Validate/test by performing output to console
+            // review/validate/test by performing output to console
             System.out.println(modelRepository.listAll());
             System.out.println(modelRepository.listAllRoles());
         };
