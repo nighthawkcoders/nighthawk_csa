85b8ed25d167b55ec274f8460d44a0adaca79cfd
John Mortensen <jmortensen@powayusd.com>
2022-05-22 07:53:55 -0700

    write files to static directories

--- src/main/java/com/nighthawk/csa/mvc/content/ContentUploadController.java
+++ src/main/java/com/nighthawk/csa/mvc/content/ContentUploadController.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.List;
 
 @Controller
@@ -27,14 +31,25 @@ public class ContentUploadController {
     }
 
     @PostMapping("/mvc/content/upload")
-    public String mvcUpload(@RequestParam("filename") MultipartFile formFile, Model modelMap) {
+    public String mvcUpload(@RequestParam("filename") MultipartFile formFile, Model modelMap)  {
         Upload repoFile = new Upload();
+        String filePath = "src/main/resources/static/images/uploads/";
+        String webPath = "/images/uploads/";
 
-        repoFile.setFile(formFile.getOriginalFilename());
+        repoFile.setFile(webPath + formFile.getOriginalFilename());
         repoFile.setType(formFile.getContentType());
         repoFile.setSize(formFile.getSize());
 
-        repo.save(repoFile);
+        try {
+            // Get the file and save it somewhere
+            Path path = Paths.get(filePath + formFile.getOriginalFilename());
+            byte[] bytes = formFile.getBytes();
+            Files.write(path, bytes);
+            repo.save(repoFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Redirect to next step
         return "redirect:/mvc/content";
