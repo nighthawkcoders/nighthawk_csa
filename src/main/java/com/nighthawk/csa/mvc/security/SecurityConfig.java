c955e604e2d63026c9de8cf789c86b5ad6b4014f
John Mortensen <jmortensen@powayusd.com>
2022-06-13 08:18:42 -0700

    lint from previous

--- src/main/java/com/nighthawk/csa/mvc/security/SecurityConfig.java
+++ src/main/java/com/nighthawk/csa/mvc/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
