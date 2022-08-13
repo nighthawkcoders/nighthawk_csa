777d2982a2f769f77d2d179bc69473adac7f5c8e
John Mortensen <jmortensen@powayusd.com>
2022-08-13 06:28:52 -0700

    remove warnings

--- src/main/java/com/nighthawk/csa/mvc/typingGame/typingAPI.java
+++ src/main/java/com/nighthawk/csa/mvc/typingGame/typingAPI.java
@@ -1,11 +1,7 @@
 package com.nighthawk.csa.mvc.typingGame;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
--- src/main/java/com/nighthawk/csa/mvc/typingGame/typingController.java
+++ src/main/java/com/nighthawk/csa/mvc/typingGame/typingController.java
@@ -1,10 +1,7 @@
 package com.nighthawk.csa.mvc.typingGame;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class typingController {
